18e5c4babc6dc72ef98ddf62b8c5cddef7ccc814
varunjram <varunram.66@gmail.com>
2022-01-20 07:12:21 +0530

    added translation URL

--- app.js
+++ app.js
@@ -2,15 +2,41 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-Input")
 var outputDiv = document.querySelector("#output")
 
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
-function clickHandler () {
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
-    outputDiv.innerText = "ccdfcdc " + txtInput.value
+function getTranslationURL(input) {
+
+    return serverURL + "?" + "text=" + input
+
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+
+    alert("somthing went wron please try again after some time")
 
 
 }
 
 
-btnTranslate.addEventListener("click" , clickHandler )
+function clickHandler() {
 
+    // outputDiv.innerText = "ccdfcdc " + txtInput.value
 
+    var inputText = txtInput.value;
+
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+
+                var translatedText = json.contents.translated;
+                outputDiv.innerText = translatedText;
+                //  console.log(json.contents.translated)})
+            })
+            .catch(errorHandler)
+
+}
+
+            btnTranslate.addEventListener("click", clickHandler)
