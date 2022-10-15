8930503f0a87be6d636c6a3781716311715fcbbb
varunjram <varunram.66@gmail.com>
2022-10-15 22:25:46 +0530

    styles

--- app.js
+++ app.js
@@ -1,42 +1,34 @@
 var btnTranslate = document.querySelector("#btn-translate");
-var txtInput = document.querySelector("#txt-Input")
-var outputDiv = document.querySelector("#output")
+var txtInput = document.querySelector("#txt-Input");
+var outputDiv = document.querySelector("#output");
 
 // var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
-var serverURL = "https://api.funtranslations.com/translate/minion.json"
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
 function getTranslationURL(input) {
-
-    return serverURL + "?" + "text=" + input
-
+  return serverURL + "?" + "text=" + input;
 }
 
 function errorHandler(error) {
-    console.log("error occured", error);
+  console.log("error occured", error);
 
-    alert("somthing went wron please try again after some time")
-
-
+  alert("somthing went wron please try again after some time");
 }
-
 
 function clickHandler() {
+  // outputDiv.innerText = "ccdfcdc " + txtInput.value
 
-    // outputDiv.innerText = "ccdfcdc " + txtInput.value
+  var inputText = txtInput.value;
 
-    var inputText = txtInput.value;
-
-    fetch(getTranslationURL(inputText))
-        .then(response => response.json())
-        .then(json => {
-
-                var translatedText = json.contents.translated;
-                outputDiv.innerText = translatedText;
-                //  console.log(json.contents.translated)})
-            })
-            .catch(errorHandler)
-
+  fetch(getTranslationURL(inputText))
+    .then((response) => response.json())
+    .then((json) => {
+      var translatedText = json.contents.translated;
+      outputDiv.innerText = translatedText;
+      //  console.log(json.contents.translated)})
+    })
+    .catch(errorHandler);
 }
 
-            btnTranslate.addEventListener("click", clickHandler)
+btnTranslate.addEventListener("click", clickHandler);
