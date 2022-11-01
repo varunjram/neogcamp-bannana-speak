6b784e3d151ba2eb7ac9c3825c4aac8578588e44
varunjram <varunram.66@gmail.com>
2022-11-01 18:35:42 +0530

    refactoring

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-Input");
 var outputDiv = document.querySelector("#output");
 
-// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
-
 var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
 function getTranslationURL(input) {
@@ -12,23 +10,18 @@ function getTranslationURL(input) {
 
 function errorHandler(error) {
   console.log("error occured", error);
-
   alert("somthing went wron please try again after some time");
 }
 
 function clickHandler() {
-  // outputDiv.innerText = "ccdfcdc " + txtInput.value
-
   var inputText = txtInput.value;
-
   fetch(getTranslationURL(inputText))
     .then((response) => response.json())
     .then((json) => {
       var translatedText = json.contents.translated;
       outputDiv.innerText = translatedText;
-      //  console.log(json.contents.translated)})
     })
-    .catch(errorHandler);
+    .catch((error) => errorHandler(error));
 }
 
 btnTranslate.addEventListener("click", clickHandler);
